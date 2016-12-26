ce4bc36d68495e186697950bde330c9326a0df25
swog <sanicsupersonic@gmail.com>
2016-12-26 15:47:03 -0600

    Update sv_fllib.lua

--- FG/gamemode/fllib/sv_fllib.lua
+++ FG/gamemode/fllib/sv_fllib.lua
@@ -2,26 +2,26 @@
 
 flLib = {};
 
-local function CheckFile()
-	if (!file.Exists("currency.txt", "DATA")) then
-		file.Write("currency.txt", "");
+local function CheckFile(name)
+	if (!file.Exists(name, "DATA")) then
+		file.Write(name, "");
 	end
 end
 
-function flLib:Append(guid, amount) CheckFile();
-	local contents = util.JSONToTable(file.Read("currency.txt", "DATA"));
+function flLib:Append(name, guid, value) CheckFile(name);
+	local contents = util.JSONToTable(file.Read(name, "DATA"));
 	
-	contents[guid] = amount;
+	contents[guid] = value;
 	
-	file.Write("currency.txt", util.TableToJSON(contents, true));
+	file.Write(name, util.TableToJSON(contents, true));
 end
 
-function flLib:Gather() CheckFile();
-	local contents = util.JSONToTable(file.Read("currency.txt", "DATA"));
+function flLib:Gather(name) CheckFile(name);
+	local contents = util.JSONToTable(file.Read(name, "DATA"));
 
 	return contents;
 end
 
 function flLib:CurrencyByID(guid)
-	return flLib:Gather()[guid]
+	return flLib:Gather()[guid];
 end
