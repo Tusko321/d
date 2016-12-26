d4a4526692e5a718d0a8f78d89010b892095e9cb
swog <sanicsupersonic@gmail.com>
2016-12-25 21:35:11 -0600

    Update sv_currency.lua

--- FG/gamemode/currency/sv_currency.lua
+++ FG/gamemode/currency/sv_currency.lua
@@ -2,9 +2,14 @@ local Currency = {};
 
 hook.Add("PlayerInitialSpawn", "currency", function(ply)
 	Currency[ply:SteamID()] = 0;
+	ply:SetNWInt("currency", 0);
 end);
 
 local meta = FindMetaTable("Player");
 function meta:GetCurrency()
 	return Currency[self:SteamID()];	
+end
+function meta:SetCurrency(amount)
+	Currency[self:SteamID()] = amount;	
+	ply:SetNWInt("currency", amount);
 end
