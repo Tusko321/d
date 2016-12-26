37ca165afe8eae3216e63734f301ee36b58d678d
swog <sanicsupersonic@gmail.com>
2016-12-25 23:17:34 -0600

    Create sv_fllib.lua

---
+++ FG/gamemode/fllib/sv_fllib.lua
@@ -0,0 +1,21 @@
+flLib = {};
+
+local function CheckFile()
+	if (!file.Exists("currency.txt", "DATA")) then
+		file.Write("currency.txt", "");
+	end
+end
+
+function flLib:Append(guid, amount) CheckFile();
+	local contents = util.JSONToTable(file.Read("currency.txt", "DATA"));
+	
+	contents[guid] = amount;
+	
+	file.Write("currency.txt", util.TableToJSON(contents, true));
+end
+
+function flLib:Gather() CheckFile();
+	local contents = util.JSONToTable(file.Read("currency.txt", "DATA"));
+
+	return contents;
+end
