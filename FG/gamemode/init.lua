233e43e290e9093ca812ade12937e1e8e8fc54b8
swog <sanicsupersonic@gmail.com>
2016-12-25 21:25:47 -0600

    Update init.lua

--- FG/gamemode/init.lua
+++ FG/gamemode/init.lua
@@ -5,6 +5,9 @@ include("shared.lua")
 AddCSLuaFile("spawn_in/cl_spawn_in.lua")
 include("spawn_in/sv_spawn_in.lua")
 
+AddCSLuaFile "link/cl_link.lua";
+include "sv_link.lua";
+
 GM.Config = GM.Config or {}
 
 function GM:PlayerConnect(name)
