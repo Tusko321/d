964ca103ee7547c278eab0fc3e069bc098f15446
swog <sanicsupersonic@gmail.com>
2016-12-25 21:22:31 -0600

    Create sv_link.lua

---
+++ FG/gamemode/link/sv_link.lua
@@ -0,0 +1,11 @@
+function bcChat(...)
+	net.Start("bcChat");
+	net.WriteTable({...});
+	net.Broadcast();
+end
+
+function bcPrint(str)
+	for k,v in next, player.GetAll() do
+		v:PrintMessage(HUD_PRINTCONSOLE, str);
+	end
+end
